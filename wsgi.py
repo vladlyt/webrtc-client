b005bb07a88c76cbe6230977ea710e201e561b28
Vlad Lytvynenko <sir.sagramor@gmail.com>
2021-05-02 22:11:37 +0300

    Added favicon and TODOs

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
-
 
 PORT = os.environ.get('PORT', 5000)
 app = Flask(__name__)
 
 
+# TODO add possibility to mute/stop video
+# TODO add rooms per uuid
+# TODO show name of the user?
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        directory=os.path.join(app.root_path, 'static'),
+        filename='favicon.ico',
+        mimetype='image/vnd.microsoft.icon',
+    )
 
 
 if __name__ == '__main__':
