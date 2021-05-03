1e89885da7d920b00d01e5eaa116fa18a5558e9e
Vlad Lytvynenko <sir.sagramor@gmail.com>
2021-05-03 12:38:24 +0300

    Fix

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
 var config = {
-    // host: 'https://webrtc-express.herokuapp.com'
+    host: 'https://webrtc-express.herokuapp.com'
     // for local development
-    host: 'http://localhost:3000'
+    // host: 'http://localhost:3000'
 }
